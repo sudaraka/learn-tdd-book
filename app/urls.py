97110d651a687250e070273b6c68b630796fcc36
Sudaraka Wijesinghe <sudaraka.wijesinghe@gmail.com>
2014-02-16 23:35:26 +0530

    End of chapter 6

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,6 @@
 """ App url definitions """
 
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, url, include
 
 from django.contrib import admin
 admin.autodiscover()
@@ -8,9 +8,7 @@ admin.autodiscover()
 urlpatterns = patterns(
     '',
     url(r'^$', 'lists.views.home_page', name='home'),
-    url(r'^lists/(\d+)/$', 'lists.views.view_list', name='view_list'),
-    url(r'^lists/(\d+)/new_item$', 'lists.views.add_item', name='add_item'),
-    url(r'^lists/new$', 'lists.views.new_list', name='new_list'),
+    url(r'^lists/', include('lists.urls')),
     # url(r'^blog/', include('blog.urls')),
 
     #url(r'^admin/', include(admin.site.urls)),
---
+++ lists/urls.py
@@ -0,0 +1,10 @@
+""" List url definitions """
+
+from django.conf.urls import patterns, url
+
+urlpatterns = patterns(
+    'lists',
+    url(r'^(\d+)/$', 'views.view_list', name='view_list'),
+    url(r'^(\d+)/new_item$', 'views.add_item', name='add_item'),
+    url(r'^new$', 'views.new_list', name='new_list'),
+)
