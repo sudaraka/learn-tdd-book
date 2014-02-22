4ec010bf55776bcb8ab618739c406036438e90ce
Sudaraka Wijesinghe <sudaraka.wijesinghe@gmail.com>
2014-02-22 18:06:46 +0530

    Hacked FT runner to be able to test staging

--- ft/tests.py
+++ ft/tests.py
@@ -1,5 +1,7 @@
 """ Functional test for TDD Book. """
 
+import sys
+
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -7,6 +9,26 @@ from selenium.webdriver.common.keys import Keys
 
 class NewUserTest(LiveServerTestCase):
     """ Test user experience on first visit to the site. """
+
+    @classmethod
+    def setUpClass(cls):
+        """ Initialize test/live servers """
+
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+
+                return
+
+        LiveServerTestCase.setUpClass()
+        cls.server_url = cls.live_server_url
+
+    @classmethod
+    def tearDownClass(cls):
+        """ Cleanup global test/live test environment """
+
+        if cls.server_url == cls.live_server_url:
+            LiveServerTestCase.tearDownClass()
 
     def setUp(self):
         """ Initialize environment for each test """
@@ -27,7 +49,7 @@ class NewUserTest(LiveServerTestCase):
 
         # Edith has heard about a cool new online to-do app. She goes to
         # checkout its home page.
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
 
         # She notice the page title and header mention to-do lists.
         self.assertIn('To-Do', self.browser.title)
@@ -73,7 +95,7 @@ class NewUserTest(LiveServerTestCase):
         self.browser = webdriver.Firefox()
 
         # Francis visits the home page and there's no sign of Edith's list
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
 
         self.assertNotIn('Buy peacock feathers', page_text)
@@ -103,7 +125,7 @@ class NewUserTest(LiveServerTestCase):
         """ Test to ensure we have the basic layout and style """
 
         # Edith goes to the home page
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         #self.browser.set_window_size(1024, 768)
         viewport = self.browser.get_window_size()
 
