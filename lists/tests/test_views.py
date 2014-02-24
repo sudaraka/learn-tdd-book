d5a8bd5eeb25859a8c3bad1ec39d2c363e38a950
Sudaraka Wijesinghe <sudaraka.wijesinghe@gmail.com>
2014-02-24 12:17:10 +0530

    Split unti test into two files

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -28,33 +28,6 @@ class HomePageTest(TestCase):
         self.assertEqual(response.content.decode(), expected_html)
 
 
-class ListAndItemModelTest(TestCase):
-    """ Test Item model related operations """
-
-    def test_saving_and_retrieving_items(self):
-        """ Test saving and retrieving items """
-
-        list_ = List()
-        list_.save()
-
-        first_item = Item()
-        first_item.text = 'First (ever) list item'
-        first_item.list = list_
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = 'Item the second'
-        second_item.list = list_
-        second_item.save()
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-        self.assertEqual(saved_items[0].text, first_item.text)
-        self.assertEqual(saved_items[0].list, list_)
-        self.assertEqual(saved_items[1].text, second_item.text)
-        self.assertEqual(saved_items[1].list, list_)
-
-
 class ListViewTest(TestCase):
     """ List view UI (url, view, template) test """
 
